1a8d7fd04ac06c8f84a046ba44772ca8fe10255e
Söùrãv <souravpramanick1997@gmail.com>
2025-02-27 18:03:07 +0530

    user route - signup signin updated

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const UserModel = require("../models/user.model");
-const ContactsModel = require("../models/contacts.model")
+const ContactsModel = require("../models/contacts.model");
 const VerificationTokenModel = require("../models/verifyToken.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -8,7 +9,7 @@ const sendVerificationMail = require("../config/nodemailer.config");
 
 const userRouter = express.Router();
 const JWT_SECRET_VERIFICATION_TOKEN = process.env.JWT_SECRET_VERIFICATION_TOKEN;
-const JWT_SECRET_SIGN_IN = process.env.JWT_SECRET_SIGN_IN
+const JWT_SECRET_SIGN_IN = process.env.JWT_SECRET_SIGN_IN;
 userRouter.post("/find-user", async (req, res) => {
   const { email } = req.body;
 
@@ -59,22 +60,27 @@ userRouter.post("/find-username", async (req, res) => {
 });
 
 userRouter.post("/signup", async (req, res) => {
+  const session = await mongoose.startSession();
   try {
+    
     const {
       email,
       username,
       password,
-      
+
       findByEmail,
       first_name,
       last_name,
     } = req.body;
-    if (!email || !username || !password )
+    if (!email || !username || !password)
       return res.status(400).json({ message: "Invalid request body" });
     const user = await UserModel.findOne({ email });
     if (user)
       return res.status(409).json({ message: "User already registered" });
-    const hash = bcrypt.hashSync(password, 5);
+    const hash = await bcrypt.hash(password, 5);
+    session.startTransaction()
+    const userContact = new ContactsModel({ contacts: [] });
+    await userContact.save({ session });
     const newUser = new UserModel({
       email,
       username,
@@ -82,29 +88,34 @@ userRouter.post("/signup", async (req, res) => {
       password: hash,
       first_name,
       last_name,
+      // image:null,
+      contacts: userContact._id,
       verified: false,
     });
-    const userContacts = new ContactsModel({username,contacts:[]})
-    await newUser.save();
-    await userContacts.save()
+
+    await newUser.save({ session });
     const token = jwt.sign(
       { email, username, id: newUser._id },
       JWT_SECRET_VERIFICATION_TOKEN,
       { expiresIn: "1d" }
     );
     const verificationToken = new VerificationTokenModel({ token, email });
-    await verificationToken.save();
-    const response = await sendVerificationMail(email, token);
+    await verificationToken.save({ session });
+    // await sendVerificationMail(email, token);
+    await session.commitTransaction();
     return res
       .status(201)
       .json({ message: "Sign up success, please verify your email." });
   } catch (error) {
-    console.log(error);
+    await session.abortTransaction();
     return res.status(500).json({ message: error.message });
+  } finally {
+    await session.endSession()
   }
 });
 userRouter.post("/verify-email/:token", async (req, res) => {
   const { token } = req.params;
+  const session = await mongoose.startSession()
   try {
     const findToken = await VerificationTokenModel.findOne({ token });
     if (!findToken) {
@@ -118,13 +129,11 @@ userRouter.post("/verify-email/:token", async (req, res) => {
             if (err) {
               if (err.name === "TokenExpiredError") {
                 const decoded = jwt.decode(token);
-                return res
-                  .status(401)
-                  .json({
-                    message: "Token Expired",
-                    data: decoded,
-                    state: { isExpired: true },
-                  });
+                return res.status(401).json({
+                  message: "Token Expired",
+                  data: decoded,
+                  state: { isExpired: true },
+                });
               } else {
                 return res
                   .status(401)
@@ -132,18 +141,23 @@ userRouter.post("/verify-email/:token", async (req, res) => {
               }
             } else {
               try {
+                 session.startTransaction()
                 await UserModel.findByIdAndUpdate(decoded.id, {
                   verified: true,
-                });
-                await VerificationTokenModel.findByIdAndDelete(findToken._id);
+                }).session(session);
+                await VerificationTokenModel.findByIdAndDelete(findToken._id).session(session);
+                await session.commitTransaction()
                 return res.json({
                   message: "User verified",
                   state: { isVerified: true },
                 });
               } catch (error) {
+                await session.abortTransaction()
                 return res
                   .status(500)
                   .json({ message: error.message, state: { isError: true } });
+              }finally{
+                 session.endSession()
               }
             }
           }
@@ -170,19 +184,26 @@ userRouter.post("/signin", async (req, res) => {
           if (err) return res.status(500).json({ message: err.message });
           else if (!match)
             return res.status(401).json({ message: "Incorrect password" });
-          const token = jwt.sign({
-            id: user._id,
-            username: user.username,
-            email,
-          },JWT_SECRET_SIGN_IN,{expiresIn:"1d"});
+          const token = jwt.sign(
+            {
+              mongoId: user._id,
+              username: user.username,
+              email,
+            },
+            JWT_SECRET_SIGN_IN,
+            { expiresIn: "1d" }
+          );
           return res.json({
             message: "Login success",
             data: {
+              mongoId: user._id,
               email,
               token,
               username: user.username,
               first_name: user.first_name,
               last_name: user.last_name,
+              image:user.image,
+              contactsId:user.contacts
             },
           });
         });
